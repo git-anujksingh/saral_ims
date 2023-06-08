912a905f08142fba9fc8f825197cfdec036701d4
ANUJ KUMAR SINGH <anujk01997@gmail.com>
2023-06-08 09:06:22 +0530

    _Env_HotFix

--- src/app/features/masters/product-category/shared/product-category.service.ts
+++ src/app/features/masters/product-category/shared/product-category.service.ts
@@ -2,7 +2,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Collection } from 'src/app/common/collection';
-import { Environment} from './../../../../../environments/environment';
+import { Environment} from './../../../../../environments/environment.prod';
 
 @Injectable({
   providedIn: 'root'
